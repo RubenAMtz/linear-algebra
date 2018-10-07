cd4a4c5c5363339bd9cd6cd7d96de23ddb3b2a4c
Jesus <alv.mtz94@gmail.com>
2018-10-07 13:35:10 +0200

    added descriptive comments

--- linear-algebra.py
+++ linear-algebra.py
@@ -3,6 +3,14 @@ import math
 class Vector:
   # Constructor
   def __init__(self, components, set_unit_vectors = True):
+    """
+    Constructs a Vector object by passing the components of a "vector" in a list
+
+    @param components: A list containing the numeric components\n
+    @type components: list\n
+    @param set_unit_vectors: A flag to calculate a unit vector object of the Vector\n
+    @type set_unit_vectors: bool
+    """
     self._components = components
     self._magnitude = self.calc_magnitude(components)
     self._dim = self.calc_dim()
@@ -37,6 +45,11 @@ class Vector:
   def __add__(self, vector): # = +(a,b)
     """
     Adding vectors, creates a vector with a new magnitud and direction
+    
+    @param vector: a vector\n
+    @type vector: Vector\n
+    @return: vector + vector\n
+    @rtype: Vector
     """
     if not self.valid_dims(vector):
       raise ValueError("Vector Dimensions don't Match.")
@@ -51,7 +64,12 @@ class Vector:
 
   def __sub__(self, vector): # = -(a,b)
     """
-    Substracting vectors, creates a vector with a new magnitud and direction
+    Substracting vectors, creates a vector with a new magnitud and direction.
+    
+    @param vector: a vector\n
+    @type vector: Vector\n
+    @return: vector + vector\n
+    @rtype: Vector
     """
     if not self.valid_dims(vector):
       raise ValueError("Vector Dimensions don't Match.")
@@ -67,18 +85,34 @@ class Vector:
   def calc_dim(self):
     """
     Calculates the number of dimensions of the vector (number of elements)
+    
+    @return: dimensions of the vector\n
+    @rtype: int
     """
     return len(self.components)
 
-  def calc_magnitude(components):
+  def calc_magnitude(self, components):
     """
     Calculates the distance or magnitude of the vector(number of elements)
+    
+    @param components: components of a vector\n
+    @type components: list\n
+    @return: magnitude of vector\n
+    @rtype: float
     """
-    magnitude = sum([x**2 for x in components])
+    magnitude = sum([x ** 2 for x in components])
     magnitude = math.sqrt(magnitude)
     return magnitude
 
   def valid_dims(self, vector):
+    """
+    Compares the dimensions between Vector objects
+    
+    @param vector: A Vector object (to be compared with)
+    @type: Vector object
+    return: TRUE/FALSE
+    rtype: Bool        
+    """
     return self.dimensions == vector.dimensions
 
   def __mul__(self, other):
@@ -86,6 +120,7 @@ class Vector:
     Multiplies a vector with a scalar
     This multiply a vector with a numeric value and returns a vector with a different magnitud. 
     The direction will be shifted if the scalar brings a negative sign.        
+    
     @param other: a scalar\n
     @type other: float or int\n
     @return: Vector * scalar\n
@@ -96,13 +131,47 @@ class Vector:
 
   #rmul to prevent for order of factors in parameters
   def __rmul__(self, other):
+    """
+    Same as method __mul__(self,other)
+    """    
     if type(other) == int or type(other) == float:
       return Vector([x * other for x in self.components])
 
-v = Vector([1,2])
-w = Vector([3,4])
-v.dot(w)
-Vector.dot(v,w)
+  def calc_unit_vectors(self):
+    """
+    Calculates the unit vectors of the vector
+    
+    return: A list of Vector type objects
+    rtype: List (of Vector objects)
+    """
+    result = []
+    for i in range(self.dimensions):
+      unit_vector = [0] * self.dimensions
+      unit_vector[i] = 1
+      result.append(Vector(unit_vector, set_unit_vectors=False))
+    return result
+
+  # Static methods
+  @staticmethod
+  def dot(a, b):
+    """ 
+    Calculates the dot product between two Vector type objects a & b
+    @param a: A Vector object
+    @param b: A Vector object
+    return: A scalar
+    rtype: Float    
+    """
+    return sum([x * y for x, y in zip(a.components, b.components)])
+
+
+v = Vector([1,2,3])
+w = Vector([1,2,3])
+print(v.unit_vectors[0].components)
+
+print(Vector.dot(w, v))
+# v = Vector([1,2])
+# w = Vector([3,4])
+# Vector.dot(v,w)
 
 # v = Vector([4,5])
 # w = Vector([1,1])
