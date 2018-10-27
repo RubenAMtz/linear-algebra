625c7c6cc5ff6fb58a7789c2c65d3cbafc0ed3c2
RubenAMtz <ruben.amtz@outlook.com>
2018-10-27 10:07:05 +0100

    added polar_2d, fixed unit vector and added versors

--- vector.py
+++ vector.py
@@ -4,30 +4,30 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 class Vector:
   # Constructor
-  def __init__(self, components, set_unit_vectors = True, log = True):
+  def __init__(self, components, set_versors = True, log = True):
     """
     Constructs a Vector object by passing the components of a "vector" in a list
 
     @param components: A list containing the numeric components\n
     @type components: list\n
-    @param set_unit_vectors: A flag to calculate a unit vector object of the Vector\n
-    @type set_unit_vectors: bool
+    @param set_versors: A flag to calculate the orthogonal unit vector objects of the Vector\n
+    @type set_versors: bool
     return: A vector
     rtype: Vector
     """
     self._components = components
     self._magnitude = self.calc_magnitude(components)
     self._dim = self.calc_dim()
-    if set_unit_vectors:
-      self._unit_vectors = self.calc_unit_vectors()
+    if set_versors:
+      self._versors = self.calc_versors()
     # logging
     if log:
       logging.info('Created vector - %s', str(self.components))
       
   # Getters
   @property
-  def unit_vectors(self):
-    return self._unit_vectors
+  def versors(self):
+    return self._versors
   
   @property #decorator
   def components(self):
@@ -47,7 +47,7 @@ class Vector:
     self._components = components
     self._dim = self.calc_dim()
     self._magnitude = self.calc_magnitude(components)
-    self._unit_vectors = self.calc_unit_vectors()
+    self._versors = self.calc_versors()
 
   # Instance methods
   def __add__(self, vector): # = +(a,b)
@@ -141,21 +141,21 @@ class Vector:
     @rtype: float
     """
     magnitude = sum([x ** 2 for x in components])
-    magnitude = math.sqrt(magnitude)
+    magnitude = round(math.sqrt(magnitude), 15)
     return magnitude
 
-  def calc_unit_vectors(self):
+  def calc_versors(self):
     """
-    Calculates the unit vectors of the vector
+    Calculates the versors (orthogonal unit vectors) given by the vector
     
     return: A list of Vector type objects
     rtype: List (of Vector objects)
     """
     result = []
     for i in range(self.dimensions):
-      unit_vector = [0] * self.dimensions
-      unit_vector[i] = 1
-      result.append(Vector(unit_vector, set_unit_vectors=False, log=False))
+      versor = [0] * self.dimensions
+      versor[i] = 1
+      result.append(Vector(versor, set_versors=False, log=False))
     return result
 
   def valid_dims(self, vector):
@@ -195,13 +195,81 @@ class Vector:
     return: true if vectors are collinear
     rtype: bool
     """
-    result = round(Vector.dot(a, b) / (a.magnitude * b.magnitude), 10)
+    result = round(Vector.dot(a, b) / (a.magnitude * b.magnitude), 15)
+    #result = Vector.dot(a, b) / (a.magnitude * b.magnitude)
     if result == -1:
       logging.warning("Collinear with opposite directions (180 deg)")
     
-    results = round(1.0, 10) == abs(result)
+    #results = round(1.0, 10) == abs(result)
+    results = 1.0 == abs(result)
     if results:
       logging.info("Collinearity - %s is colinear with %s", str(a.components), str(b.components))
     else:
       logging.info("Collinearity - %s is not colinear with %s", str(a.components), str(b.components))
     return results
+
+  @staticmethod
+  def angle(a, b, degrees=True):
+    """
+    Calculates angle between two vectors    
+    """
+    result = round(Vector.dot(a, b) / (a.magnitude * b.magnitude), 10)
+    if degrees:
+      return math.degrees(math.acos(result))
+    return math.acos(result)
+
+  def __str__(self):
+    string = str(self.components)
+    return string
+
+  @staticmethod
+  def polar_2d(a, degrees = True):
+    if a.dimensions > 2:
+      raise ValueError("Vector has more than 2 dimensions")
+    #assert a.dimensions == 2
+    x, y = a.components
+    #sum = 0
+    # 1st quadrant
+    if x >= 0 and y >= 0:
+      sum = 0
+    # 2nd & 3rd quadrant
+    elif x <= 0:
+      sum = 180
+    # 4th quadrant
+    elif x >= 0 and y <= 0:
+      sum = 360  
+    
+    if x == 0:
+      x = 0.0000001
+    
+    angle = math.atan(y / x) + math.radians(sum)
+    if degrees:
+        angle = math.degrees(angle)
+    return a.magnitude, angle
+
+  @staticmethod
+  def cross_product(a, b):
+    # product of magnitud of vectors * sin(angle between them)
+    pass
+
+
+  def calc_unit_vector(self):
+    return Vector([component/self.magnitude for component in self.components])
+
+def tryouts():
+  a = Vector([5, 3])
+  b = Vector([-2, 3.3333])
+  d = Vector([6, -9.9999])
+  print(Vector.angle(a,b))
+  print(a)
+  print(Vector.polar_2d(b))
+  print(Vector.polar_2d(d))
+  print(a.calc_versors()[0])
+  print(a.calc_versors()[1])
+  unit_a = a.calc_unit_vector()
+  print(a.magnitude)
+  print(unit_a)
+  print(unit_a.magnitude)
+  print(Vector.collinear(b,d))
+
+tryouts()
